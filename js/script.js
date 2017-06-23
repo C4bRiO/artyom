ae758e2f2803a5f0f2ef0359b753b60151a19578
C4bRiO <everleiva21@gmail.com>
2017-06-23 14:22:32 -0400

    first js testings

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
    $('#btn_iniciar').click(function(){
-   		alert('Escucha iniciada');
+   		//alert('Escucha iniciada');
+   		artyom.on(['Good morning']).then(function(i){
+        alert("Good morning ! How are you?");
+    });
    });
 
       $('#btn_finalizar').click(function(){
