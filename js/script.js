9baf0b56802476f05c2f547053a367b48d20b98a
C4bRiO <everleiva21@gmail.com>
2017-06-23 14:15:40 -0400

    js implementado

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+   $('#btn_iniciar').click(function(){
+   		alert('Escucha iniciada');
+   });
+
+      $('#btn_finalizar').click(function(){
+   		alert('Escucha finalizada');
+   });
+});
