e769241ecc670913157e1965fc99f43c838e62b6
C4bRiO <everleiva21@gmail.com>
2017-06-23 14:26:20 -0400

    boton de inicio, ver consola

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@ $(document).ready(function(){
 
    $('#btn_iniciar').click(function(){
    		//alert('Escucha iniciada');
-   		artyom.on(['Good morning']).then(function(i){
-        alert("Good morning ! How are you?");
+   		
+   		function startContinuousArtyom(){
+    	artyom.fatality();// Detener cualquier instancia previa
+
+    	setTimeout(function(){// Esperar 250ms para inicializar
+        	artyom.initialize({
+        		lang:"en-GB",// Más lenguajes son soportados, lee la documentación
+		        continuous:true,// Artyom obedecera por siempre
+		        listen:true, // Iniciar !
+		        debug:true, // Muestra un informe en la consola
+		        speed:1 // Habla normalmente
+		    }).then(function(){
+        	
+        	console.log("Ready to work !");
+       	 	});
+    	},250);
+}
     });
    });
 
